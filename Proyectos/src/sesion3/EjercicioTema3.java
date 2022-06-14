e1c80103d74ce9896db66a1d910700f0532a8814
d3stroya <destroya@users.noreplay.github.com>
2022-06-14 16:41:38 +0200

    ejercicio tema 3

---
+++ Proyectos/src/sesion3/EjercicioTema3.java
@@ -0,0 +1,12 @@
+package sesion3;
+
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+        // Crear un bucle que permita concatenar textos e imprima el resultado final por consola.
+        String[] nombres = {"Franky", "Bridget", "Allie"};
+
+        for(String nombre : nombres) {
+            System.out.println(nombre);
+        }
+    }
+}
