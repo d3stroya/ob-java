d68c77b3f2e670f0cec44701376af2a0cdc2ba41
d3stroya <destroya@users.noreplay.github.com>
2022-06-14 16:52:25 +0200

    ejercicio 3 versión 2

--- Proyectos/src/sesion3/EjercicioTema3.java
+++ Proyectos/src/sesion3/EjercicioTema3.java
@@ -1,12 +1,30 @@
 package sesion3;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Map.Entry;
+
 public class EjercicioTema3 {
     public static void main(String[] args) {
         // Crear un bucle que permita concatenar textos e imprima el resultado final por consola.
-        String[] nombres = {"Franky", "Bridget", "Allie"};
+        Map<Integer, String> nombres = new HashMap<>();
 
-        for(String nombre : nombres) {
-            System.out.println(nombre);
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pide tu primer deseo:");
+        String deseo1 = scanner.nextLine();
+        nombres.put(1, deseo1);
+        System.out.println("Pide un segundo deseo:");
+        String deseo2 = scanner.nextLine();
+        nombres.put(2, deseo2);
+        System.out.println("Piensa bien, es tu último deseo:");
+        String deseo3 = scanner.nextLine();
+        nombres.put(3, deseo3);
+            
+        
+        for(Entry<Integer, String> peticion : nombres.entrySet()) {
+            System.out.println("Deseo " + peticion.getKey() + ": " + peticion.getValue());
+        }      
+
     }
 }
