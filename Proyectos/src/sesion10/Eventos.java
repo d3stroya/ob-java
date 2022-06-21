b72bed007f9a637d91f347fdc3a6405053e11fc1
d3stroya <destroya@users.noreplay.github.com>
2022-06-21 16:24:04 +0200

    sesion10

---
+++ Proyectos/src/sesion10/Eventos.java
@@ -0,0 +1,14 @@
+package sesion10;
+
+public class Eventos {
+    // Los eventos son algo que pasa. Hay un emisor y un receptor.
+    // Swing crea interfaces de usuario. Cuando el usuario interactúa con la interfaz, suceden eventos (clic, seleccionar, abrir y cerrar ventanas,...).
+    // Patrón observador: cuando sucede un evento, ejecuta una acción.
+    public static void main(String[] args) {
+        int entero = Integer.parseInt("21");
+        System.out.println(entero);
+    }
+
+
+
+}
